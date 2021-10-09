c5f6d62823262a4b6d6b28f911562d4c33905996
Mr.Trinh <trinhxuanvy1@gmail.com>
2021-10-09 21:24:41 +0700

    Responsive index page

--- public/javascripts/handle.js
+++ public/javascripts/handle.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
   // Xử lý menu cố định trên top
   $(function () {
-    const header = $("#header");
-    const offsetHeader = $(header).offset();
-    const topHeader = offsetHeader.top;
+    const posHeader = $("#header").position();
+    const topHeader = posHeader.top;
 
-    $(window).on("scroll", function () {
+    $(window).on("scroll load", function () {
       const topWindow = $(window).scrollTop();
 
       if (topWindow > topHeader) {
@@ -13,6 +12,21 @@ $(document).ready(function () {
       } else {
         $(header).removeClass("header--fixed");
       }
+    });
+  });
+
+  // Xử lý menu reponsive
+  $(function() {
+    $("#menu-toggle").click(function (e) { 
+      e.preventDefault();
+      const menuList = $(".header__menu-list");
+      const hasClass = $(menuList).hasClass("header__menu-list-responsive");
+      console.log(hasClass);
+      if (hasClass) {
+        $(menuList).removeClass("header__menu-list-responsive");
+      } else {
+        $(menuList).addClass("header__menu-list-responsive");
+      }    
     });
   });
 
